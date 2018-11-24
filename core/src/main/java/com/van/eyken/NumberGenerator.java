20b984becf334428fc94889f4e0f22ae66f9f2b1
joêl van eyken <j_van_eyken@hotmail.com>
2018-11-24 20:15:51 +0100

    Ioc container

---
+++ core/src/main/java/com/van/eyken/NumberGenerator.java
@@ -0,0 +1,11 @@
+package com.van.eyken;
+
+/**
+ * Created by Joël Van Eyken
+ * on 24/11/2018
+ */
+public interface NumberGenerator {
+
+    int next();
+    int getMaxNumber();
+}
---
+++ core/src/main/java/com/van/eyken/NumberGeneratorImpl.java
@@ -0,0 +1,27 @@
+package com.van.eyken;
+
+import java.util.Random;
+
+/**
+ * Created by Joël Van Eyken
+ * on 24/11/2018
+ */
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    //<editor-fold desc="Fiels">
+    private Random random = new Random();
+    private int maxNumber = 100;
+    //</editor-fold>
+
+    //<editor-fold desc="public methods">
+    @Override
+    public int next() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return 0;
+    }
+    //</editor-fold>
+}
